6d923f21b27846941c890130500a722b0317defb
Chernogorov <hristochernogorov@gmail.com>
2015-02-26 23:17:02 +0200

    Empty solution

---
+++ PingPong/PingPong.cs
@@ -0,0 +1,18 @@
+﻿//The first team project.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong
+{
+    class PingPong
+    {
+        static void Main()
+        {
+        }
+    }
+}
